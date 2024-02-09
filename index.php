47f5ab0d1e991cd67a1f94d1831085910ec7300b
Benedict Massolle <bm@presentprogressive.de>
2024-02-09 13:14:25 +0100

    wip

--- index.php
+++ index.php
@@ -2,51 +2,53 @@
 
 @include __DIR__ . '/vendor/autoload.php';
 
-use Kirby\Cms\App as Kirby;
+use Kirby\Cms\App;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Logger;
 use Psr\Log\LogLevel;
 
-function getLogger(?string $name = null): Logger
+function monolog(string $name = null): Logger
 {
-    if ($name === null) {
-        $name = option('presprog.logger.name');
-    }
-
-    $path = kirby()->root('logs');
-
-    if ($path === null) {
-        $path = kirby()->root('site') . '/logs';
-    }
-
-    $logger = new Logger($name);
-    $logger->pushHandler(new RotatingFileHandler($path . '/' . $name));
-
-    return $logger;
+    return kirby()->option('presprog.logger.default')($name);
 }
 
-Kirby::plugin('presprog/logger', [
+App::plugin('presprog/logger', [
     'options' => [
-        'name'      => 'kirby.log',
+        'name' => 'kirby.log',
         'default_log_level' => LogLevel::ERROR,
+        'default' => function (string $name = null) {
+            if ($name === null) {
+                $name = option('presprog.logger.name', 'kirby.log');
+            }
+
+            $path = kirby()->root('logs');
+
+            if ($path === null) {
+                $path = kirby()->root('site') . '/logs';
+            }
+
+            return new Logger($path . DIRECTORY_SEPARATOR . $name, [
+                new RotatingFileHandler($name),
+            ]);
+        },
     ],
 
     'siteMethods' => [
         'log' => function (string $message, ?string $level = null, ?string $name = null, array $context = []): void {
-            $logger = getLogger($name);
+            $logger = monolog($name);
 
             if (null === $level) {
-                $level = option('presprog.logger.default_log_level', LogLevel::ERROR);
+                $level = option('presprog.logger.default_log_level');
             }
 
             $logger->log($level, $message, $context);
         },
 
         'logException' => function (Throwable $exception, ?string $level = null, ?string $name = null) {
-            $logger = getLogger($name);
+            $logger = monolog($name);
 
             if (null === $level) {
-                $level = LogLevel::ERROR;
+                $level = LogLevel::CRITICAL;
             }
 
             $logger->log($level, $exception->getMessage(), ['stack_trace' => $exception->getTraceAsString()]);
